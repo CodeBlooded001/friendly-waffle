2405c12f8fac3e0324408af46288271002d2cc77
yashu <yaseswiniy@gmail.com>
2022-10-27 15:32:53 +0530

    Demonstrating suite and test tags in xml...

--- src/test/java/Demo/loan/carLoan.java
+++ src/test/java/Demo/loan/carLoan.java
@@ -3,6 +3,7 @@ package Demo.loan;
 import java.io.IOException;
 
 
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
--- src/test/java/testPages/SecHomePageTest.java
+++ src/test/java/testPages/SecHomePageTest.java
@@ -2,6 +2,7 @@ package testPages;
 
 import java.io.IOException;
 
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
